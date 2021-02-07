a873afdd33c7d02ecd05ac97c2e2934c2a53ce75
Happy Singh <happysingh23828@gmail.com>
2021-02-07 12:23:40 +0530

    buildSrc module added.

---
+++ buildSrc/src/main/java/ConfigFields.kt
@@ -0,0 +1,6 @@
+package dependencies
+
+object ConfigFields {
+    // add your config fields KEYS here for ex :
+    //    const val BASE_URL = "BASE_URL"
+}
---
+++ buildSrc/src/main/java/MavenUrls.kt
@@ -0,0 +1,8 @@
+package dependencies
+
+object MavenUrls {
+    const val FABRIC_PUBLIC = "https://maven.fabric.io/public"
+    const val JITPACK_IO = "https://jitpack.io"
+    const val COMMONS_WARE = "https://s3.amazonaws.com/repo.commonsware.com"
+    const val PLUGIN_GRADLE_M2 = "https://plugins.gradle.org/m2/"
+}
